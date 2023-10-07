e2d99944c17d198845e4c6147e93eebdb1f9654b
David Gootman <gootman@gmail.com>
2023-10-07 16:06:16 -0700

    Fix layout to avoid overflows

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ import {
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import axios, { AxiosRequestConfig } from "axios";
 import { CSSProperties, useCallback, useEffect, useState } from "react";
-import { Container, Image, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Container, Image, Nav, NavDropdown, Navbar, Stack } from "react-bootstrap";
 import Linkify from "react-linkify";
 import { format } from "timeago.js";
 import useLocalStorageState from "use-local-storage-state";
@@ -192,78 +192,80 @@ export default function App() {
   };
 
   return (
-    <Container className="d-flex flex-column" style={{ position: "absolute", inset: "5px" }}>
-      <Navbar collapseOnSelect expand="md">
-        <Container fluid>
-          <Navbar.Brand>Chatweb</Navbar.Brand>
-          <Nav>
-            <NavDropdown title={renderProvider(providers?.find((p) => p.id === providerId))}>
-              {providers?.map((provider) => (
-                <NavDropdown.Item onClick={() => updateProviderId(provider.id)}>
-                  {renderProvider(provider)}
-                  <span className="ms-2">{provider.name}</span>
-                </NavDropdown.Item>
+    <Container className="h-100">
+      <Stack className="h-100" direction="vertical">
+        <Navbar collapseOnSelect expand="md">
+          <Container fluid>
+            <Navbar.Brand>Chatweb</Navbar.Brand>
+            <Nav>
+              <NavDropdown title={renderProvider(providers?.find((p) => p.id === providerId))}>
+                {providers?.map((provider) => (
+                  <NavDropdown.Item onClick={() => updateProviderId(provider.id)}>
+                    {renderProvider(provider)}
+                    <span className="ms-2">{provider.name}</span>
+                  </NavDropdown.Item>
+                ))}
+              </NavDropdown>
+            </Nav>
+          </Container>
+        </Navbar>
+        <MainContainer responsive>
+          <Sidebar position="left" style={sidebarStyle}>
+            <ConversationList loading={conversations === undefined}>
+              {conversations?.map((conversation) => (
+                <Conversation
+                  name={conversation.name}
+                  onClick={() => setActiveConversation(conversation)}
+                  active={conversation === activeConversation}
+                  lastActivityTime={
+                    conversation.last_active ? format(conversation.last_active) : null
+                  }
+                  style={{ display: "flex" }}
+                >
+                  <Avatar src={conversation.avatar} name={conversation.name} />
+                </Conversation>
               ))}
-            </NavDropdown>
-          </Nav>
-        </Container>
-      </Navbar>
-      <MainContainer responsive>
-        <Sidebar position="left" style={sidebarStyle}>
-          <ConversationList loading={conversations === undefined}>
-            {conversations?.map((conversation) => (
-              <Conversation
-                name={conversation.name}
-                onClick={() => setActiveConversation(conversation)}
-                active={conversation === activeConversation}
-                lastActivityTime={
-                  conversation.last_active ? format(conversation.last_active) : null
-                }
-                style={{ display: "flex" }}
-              >
-                <Avatar src={conversation.avatar} name={conversation.name} />
-              </Conversation>
-            ))}
-          </ConversationList>
-        </Sidebar>
-        {activeConversation && (
-          <ChatContainer>
-            <ConversationHeader>
-              <ConversationHeader.Back onClick={() => setActiveConversation(undefined)} />
-              <Avatar src={activeConversation.avatar} name={activeConversation.name} />
-              <ConversationHeader.Content userName={activeConversation.name} />
-            </ConversationHeader>
-            <MessageList loading={messages === undefined}>
-              {messages?.map((message) => {
-                const direction = message.sender === me?.id ? "outgoing" : "incoming";
-                const sender =
-                  direction === "incoming" ? contacts?.find((c) => c.id === message.sender) : me;
-                return (
-                  <Message
-                    model={{
-                      type: "custom",
-                      sentTime: format(message.timestamp),
-                      sender: sender?.name,
-                      direction,
-                      position: "normal",
-                    }}
-                  >
-                    <Message.CustomContent>
-                      <Linkify>{message.body}</Linkify>
-                    </Message.CustomContent>
-                    <Message.Footer sender={sender?.name} sentTime={format(message.timestamp)} />
-                    {sender && <Avatar src={sender.avatar} name={sender.name} />}
-                  </Message>
-                );
-              })}
-            </MessageList>
-            <MessageInput
-              placeholder="Type message here"
-              onSend={(_, __, innerText) => sendMessage(innerText)}
-            />
-          </ChatContainer>
-        )}
-      </MainContainer>
+            </ConversationList>
+          </Sidebar>
+          {activeConversation && (
+            <ChatContainer>
+              <ConversationHeader>
+                <ConversationHeader.Back onClick={() => setActiveConversation(undefined)} />
+                <Avatar src={activeConversation.avatar} name={activeConversation.name} />
+                <ConversationHeader.Content userName={activeConversation.name} />
+              </ConversationHeader>
+              <MessageList loading={messages === undefined}>
+                {messages?.map((message) => {
+                  const direction = message.sender === me?.id ? "outgoing" : "incoming";
+                  const sender =
+                    direction === "incoming" ? contacts?.find((c) => c.id === message.sender) : me;
+                  return (
+                    <Message
+                      model={{
+                        type: "custom",
+                        sentTime: format(message.timestamp),
+                        sender: sender?.name,
+                        direction,
+                        position: "normal",
+                      }}
+                    >
+                      <Message.CustomContent>
+                        <Linkify>{message.body}</Linkify>
+                      </Message.CustomContent>
+                      <Message.Footer sender={sender?.name} sentTime={format(message.timestamp)} />
+                      {sender && <Avatar src={sender.avatar} name={sender.name} />}
+                    </Message>
+                  );
+                })}
+              </MessageList>
+              <MessageInput
+                placeholder="Type message here"
+                onSend={(_, __, innerText) => sendMessage(innerText)}
+              />
+            </ChatContainer>
+          )}
+        </MainContainer>
+      </Stack>
     </Container>
   );
 }
